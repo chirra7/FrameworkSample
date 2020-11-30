bc64290817176b710416dbc41cd195dcf0cb8ac4
Suresh K. Chirra <surchirr@in.ibm.com>
2020-11-30 09:17:25 +0530

    Folder name remaned

--- src/main/java/in/ibm/constants/Constants.java
+++ src/main/java/in/ibm/constants/Constants.java
@@ -2,7 +2,7 @@ package in.ibm.constants;
 
 public class Constants {
 
-    public static final String mobilesDevicesListJSONFolderPath = "src\\test\\resources\\mobileDevices";
+    public static final String mobilesDevicesListJSONFolderPath = "src\\test\\resources\\Devices";
     public static final String mobilesDevicesListJSONFilePath = "DevicesList.json";
 
     public static final String driversEXEFilesFolderPath = "";
